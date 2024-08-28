997304fdb71953dc55703b20318efc5b3c572e14
Include caleb.fun <70612830+oasisMystre@users.noreply.github.com>
2024-08-29 00:40:58 +0100

    feat: add telegram bot server

---
+++ servers/kamikaze/drizzle/0000_thin_tarantula.sql
@@ -0,0 +1,134 @@
+CREATE TABLE IF NOT EXISTS "users" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"phone" text,
+	"email" text NOT NULL,
+	"first_name" text NOT NULL,
+	"last_name" text NOT NULL,
+	"password" text NOT NULL,
+	"is_verified" boolean DEFAULT false NOT NULL,
+	"last_login" timestamp DEFAULT now() NOT NULL,
+	CONSTRAINT "users_email_unique" UNIQUE("email")
+);
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "apps" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"user" uuid NOT NULL,
+	"name" text NOT NULL,
+	"logo" text,
+	"origins" text[] DEFAULT ARRAY[]::text[],
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	CONSTRAINT "apps_user_name_unique" UNIQUE("user","name")
+);
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "webhooks" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"url" text NOT NULL,
+	"app" uuid NOT NULL,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	CONSTRAINT "unique_webhooks" UNIQUE NULLS NOT DISTINCT("app","url")
+);
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "payments" (
+	"id" text PRIMARY KEY NOT NULL,
+	"description" text,
+	"amount" text NOT NULL,
+	"mint" text,
+	"signature" text,
+	"customer" text NOT NULL,
+	"wallet" uuid,
+	"is_verified" boolean DEFAULT false NOT NULL,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	CONSTRAINT "payments_signature_unique" UNIQUE("signature")
+);
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "wallets" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"app" uuid NOT NULL,
+	"address" text NOT NULL,
+	"chain" text NOT NULL,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	CONSTRAINT "address_and_network" UNIQUE("app","address","chain")
+);
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "authUsers" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"uid" text NOT NULL,
+	"email" text,
+	"provider" text NOT NULL,
+	"auth" uuid NOT NULL,
+	"is_verified" boolean DEFAULT false NOT NULL,
+	"last_login" timestamp DEFAULT now() NOT NULL,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"verification_data" json,
+	CONSTRAINT "unique_id_auth" UNIQUE("id","auth")
+);
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "auths" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"app" uuid NOT NULL,
+	"methods" text[] NOT NULL,
+	"socials" text[] NOT NULL,
+	"origins" text[] NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "embeddedWallets" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"hash" text NOT NULL,
+	"user" uuid NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "apiKeys" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"secret_key" text NOT NULL,
+	"public_key" text NOT NULL,
+	"app" uuid NOT NULL,
+	CONSTRAINT "apiKeys_app_unique" UNIQUE("app")
+);
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "apps" ADD CONSTRAINT "apps_user_users_id_fk" FOREIGN KEY ("user") REFERENCES "public"."users"("id") ON DELETE cascade ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "webhooks" ADD CONSTRAINT "webhooks_app_apps_id_fk" FOREIGN KEY ("app") REFERENCES "public"."apps"("id") ON DELETE cascade ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "payments" ADD CONSTRAINT "payments_wallet_wallets_id_fk" FOREIGN KEY ("wallet") REFERENCES "public"."wallets"("id") ON DELETE set null ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "wallets" ADD CONSTRAINT "wallets_app_apps_id_fk" FOREIGN KEY ("app") REFERENCES "public"."apps"("id") ON DELETE no action ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "authUsers" ADD CONSTRAINT "authUsers_auth_auths_id_fk" FOREIGN KEY ("auth") REFERENCES "public"."auths"("id") ON DELETE cascade ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "auths" ADD CONSTRAINT "auths_app_apps_id_fk" FOREIGN KEY ("app") REFERENCES "public"."apps"("id") ON DELETE cascade ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "embeddedWallets" ADD CONSTRAINT "embeddedWallets_user_authUsers_id_fk" FOREIGN KEY ("user") REFERENCES "public"."authUsers"("id") ON DELETE cascade ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "apiKeys" ADD CONSTRAINT "apiKeys_app_apps_id_fk" FOREIGN KEY ("app") REFERENCES "public"."apps"("id") ON DELETE cascade ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
