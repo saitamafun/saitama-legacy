69357bf41f81d0f31af155e21867ff5a7428f67a
Include caleb.fun <70612830+oasisMystre@users.noreply.github.com>
2024-08-29 17:30:26 +0100

    feat: add telegram bot server

--- servers/bird/src/db/schema/botUsers.ts
+++
@@ -1,7 +0,0 @@
-import { pgTable, text } from "drizzle-orm/pg-core";
-
-export const botUsers = pgTable("botUsers", {
-  id: text("id").unique().notNull().primaryKey(),
-  username: text("username").unique("username", { nulls: "not distinct" }),
-  chatId: text("chat_id").notNull(),
-});
--- servers/bird/src/db/zod.ts
+++
@@ -1,6 +0,0 @@
-import { createInsertSchema, createSelectSchema } from "drizzle-zod";
-
-import { botUsers } from "./schema";
-
-export const insertBotUserSchema = createInsertSchema(botUsers);
-export const selectBotUserSchema = createSelectSchema(botUsers);
